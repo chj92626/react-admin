2bbfe17dc4cb044613cc0b43542a5a43756f9aa5
程华军 <18234495705@163.com>
2024-11-22 14:32:55 +0800

    添加背景图

--- src/pages/Stats/index.tsx
+++ src/pages/Stats/index.tsx
@@ -13,7 +13,6 @@ const PptxViewer: React.FC = () => {
   const [slideContents, setSlideContents] = useState<any[]>([]);
   const [selectedSlideIndex, setSelectedSlideIndex] = useState<number>(0);
   const [backgroundStyles, setBackgroundStyles] = useState<string[]>([]);
-  const [backgroundImages, setBackgroundImages] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const thumbnailContainerRef = useRef<HTMLDivElement>(null);
 
@@ -64,7 +63,6 @@ const PptxViewer: React.FC = () => {
     }
   }, [selectedSlideIndex]);
 
-  // 获取资源路径
   const getResourcePath = async (
     zip: JSZip,
     fileName: string,
@@ -73,14 +71,14 @@ const PptxViewer: React.FC = () => {
     try {
       const relFile = `ppt/slides/_rels/${fileName.split("/").pop()}.rels`;
       if (!zip.files[relFile]) {
-        console.error(`缺少 .rels 文件: ${fileName}`);
+        console.error(`缺少 .rels 文件: ${relFile}`);
         return null;
       }
 
       const relXml = await zip.files[relFile].async("string");
       const parser = new DOMParser();
       const relDoc = parser.parseFromString(relXml, "application/xml");
-      const target = relDoc
+      let target = relDoc
         .querySelector(`Relationship[Id="${embed}"]`)
         ?.getAttribute("Target");
 
@@ -89,17 +87,38 @@ const PptxViewer: React.FC = () => {
         return null;
       }
 
-      return `ppt/${target.replace("..", "")}`;
+      target = target.replace(/^\.\.\//, "").replace(/\/+/g, "/");
+
+      const fullPath = `ppt/${target}`;
+      if (!zip.files[fullPath]) {
+        console.error(`缺少图像文件: ${fullPath}`);
+        return null;
+      }
+
+      return fullPath;
     } catch (error) {
       console.error(`获取资源路径时出错: ${embed}`, error);
       return null;
     }
   };
 
-  // 处理 PPTX 文件
   const processPPTXFile = async (arrayBuffer: ArrayBuffer) => {
     try {
       const zip = await JSZip.loadAsync(arrayBuffer);
+
+      // 提取幻灯片尺寸
+      const presentationXml = await zip.files["ppt/presentation.xml"].async(
+        "string"
+      );
+      const parser = new DOMParser();
+      const presentationDoc = parser.parseFromString(
+        presentationXml,
+        "application/xml"
+      );
+
+      const sldSz = presentationDoc.getElementsByTagName("p:sldSz")[0];
+      const slideWidth = parseInt(sldSz.getAttribute("cx"), 10);
+      const slideHeight = parseInt(sldSz.getAttribute("cy"), 10);
 
       const slideFiles = Object.keys(zip.files).filter((fileName) =>
         fileName.startsWith("ppt/slides/slide")
@@ -112,42 +131,35 @@ const PptxViewer: React.FC = () => {
             slideImage: "",
             elements: [],
             backgroundColor: "",
-            backgroundImage: "",
           };
         }
 
         const slideXml = await zip.files[fileName].async("string");
-        const parser = new DOMParser();
         const slideDoc = parser.parseFromString(slideXml, "application/xml");
 
-        // 提取文本元素和样式
         const elements = [];
         const spElements = slideDoc.getElementsByTagName("p:sp");
-
-        let currentY = 10; // 用于跟踪文本的 Y 位置
+        const picElements = slideDoc.getElementsByTagName("p:pic");
+        let currentY = 10;
 
         for (let sp of spElements) {
           const spPr = sp.getElementsByTagName("p:spPr")[0];
           const xfrm = spPr?.getElementsByTagName("a:xfrm")[0];
           const off = xfrm?.getElementsByTagName("a:off")[0];
 
-          // 提取位置信息
           let x = 10;
-          let y = currentY; // 默认使用当前 Y 位置
+          let y = currentY;
 
           if (off) {
             x = parseInt(off.getAttribute("x"), 10) / 12700;
             y = parseInt(off.getAttribute("y"), 10) / 9000;
           }
-          console.log("sp---------------", sp);
 
           const tElements = sp.getElementsByTagName("a:t");
           const rPrElements = sp.getElementsByTagName("a:rPr");
 
           for (let i = 0; i < tElements.length; i++) {
             const text = tElements[i].textContent || "";
-            // console.log("rPrElements", rPrElements);
-
             const rPr = rPrElements[i];
 
             let fontSize = "20px";
@@ -155,8 +167,6 @@ const PptxViewer: React.FC = () => {
             let typeface = "Arial";
             let fontWeight = "normal";
             let fontStyle = "normal";
-            let pitchFamily = "";
-            let charset = "";
 
             if (rPr) {
               const sz = rPr.getAttribute("sz");
@@ -175,25 +185,8 @@ const PptxViewer: React.FC = () => {
               const latin = rPr.getElementsByTagName("a:latin")[0];
               if (latin) {
                 typeface = latin.getAttribute("typeface") || typeface;
-                pitchFamily = latin.getAttribute("pitchFamily") || pitchFamily;
-                charset = latin.getAttribute("charset") || charset;
               }
 
-              const ea = rPr.getElementsByTagName("a:ea")[0];
-              if (ea) {
-                typeface = ea.getAttribute("typeface") || typeface;
-                pitchFamily = ea.getAttribute("pitchFamily") || pitchFamily;
-                charset = ea.getAttribute("charset") || charset;
-              }
-
-              const cs = rPr.getElementsByTagName("a:cs")[0];
-              if (cs) {
-                typeface = cs.getAttribute("typeface") || typeface;
-                pitchFamily = cs.getAttribute("pitchFamily") || pitchFamily;
-                charset = cs.getAttribute("charset") || charset;
-              }
-
-              // 检查加粗和斜体
               if (rPr.getAttribute("b") === "1") {
                 fontWeight = "bold";
               }
@@ -202,7 +195,6 @@ const PptxViewer: React.FC = () => {
               }
             }
 
-            // 如果没有明确的 y 位置，使用 currentY 并增加行高
             if (!off) {
               y = currentY;
             }
@@ -214,71 +206,84 @@ const PptxViewer: React.FC = () => {
               typeface,
               fontWeight,
               fontStyle,
-              pitchFamily,
-              charset,
               x,
               y,
             });
 
-            // 更新 currentY，避免重叠
             const textHeight = parseInt(fontSize, 10);
-            currentY = y + textHeight + 5; // 5 是额外间距
+            currentY = y + textHeight + 5;
           }
         }
 
-        // 提取背景颜色
         const bgElement = slideDoc.getElementsByTagName("p:bg")[0];
-        let backgroundColor = "#FFFFFF";
+        let backgroundColor = "#ffffff";
         if (bgElement) {
           const solidFill = bgElement.getElementsByTagName("a:solidFill")[0];
           if (solidFill) {
             const srgbClr = solidFill.getElementsByTagName("a:srgbClr")[0];
             if (srgbClr) {
-              backgroundColor = `#${srgbClr.getAttribute("val") || "FFFFFF"}`;
+              backgroundColor = `#${srgbClr.getAttribute("val")}`;
             }
           }
         }
 
-        // 提取背景图片
-        let backgroundImage = "";
-        const blipFill = bgElement?.getElementsByTagName("p:blipFill")[0];
-        if (blipFill) {
-          const blip = blipFill.getElementsByTagName("a:blip")[0];
-          const embed = blip?.getAttribute("r:embed");
-          if (embed) {
-            const imagePath = await getResourcePath(zip, fileName, embed);
-            if (imagePath) {
-              const imageData = await zip.files[imagePath].async("base64");
-              backgroundImage = `data:image/png;base64,${imageData}`;
-            }
-          }
-        }
-
-        // 将幻灯片渲染为图片
         const canvas = document.createElement("canvas");
         const context = canvas.getContext("2d");
         if (context) {
-          // 根据幻灯片布局设置画布大小
-          canvas.width = 960; // 示例宽度
-          canvas.height = 540; // 示例高度
+          canvas.width = slideWidth / 12700; // 将EMU单位转换为像素
+          canvas.height = slideHeight / 12700;
 
-          // 填充背景颜色
           context.fillStyle = backgroundColor;
           context.fillRect(0, 0, canvas.width, canvas.height);
 
-          // 如果有背景图片，绘制背景图片
-          if (backgroundImage) {
-            const img = new Image();
-            img.src = backgroundImage;
-            await new Promise((resolve) => {
-              img.onload = () => {
-                context.drawImage(img, 0, 0, canvas.width, canvas.height);
-                resolve(null);
-              };
-            });
+          for (let pic of picElements) {
+            const blip = pic.getElementsByTagName("a:blip")[0];
+            const embed = blip?.getAttribute("r:embed");
+
+            const xfrm = pic.getElementsByTagName("a:xfrm")[0];
+            const off = xfrm?.getElementsByTagName("a:off")[0];
+            const ext = xfrm?.getElementsByTagName("a:ext")[0];
+
+            let x = 0;
+            let y = 0;
+            let width = 0;
+            let height = 0;
+
+            if (off) {
+              x = parseInt(off.getAttribute("x"), 10) / 12700;
+              y = parseInt(off.getAttribute("y"), 10) / 12700;
+            }
+
+            if (ext) {
+              width = parseInt(ext.getAttribute("cx"), 10) / 12700;
+              height = parseInt(ext.getAttribute("cy"), 10) / 12700;
+            }
+
+            if (embed) {
+              const imagePath = await getResourcePath(zip, fileName, embed);
+              if (imagePath && zip.files[imagePath]) {
+                const imageData = await zip.files[imagePath].async("base64");
+                const imageSrc = `data:image/png;base64,${imageData}`;
+
+                const img = new Image();
+                img.src = imageSrc;
+                await new Promise((resolve) => {
+                  img.onload = () => {
+                    const aspectRatio = img.width / img.height;
+                    if (width / height > aspectRatio) {
+                      width = height * aspectRatio;
+                    } else {
+                      height = width / aspectRatio;
+                    }
+                    context.drawImage(img, x, y, width, height);
+                    resolve(null);
+                  };
+                });
+              }
+            }
           }
 
-          let paddingTop = 40; // 内边距
+          let paddingTop = 40;
           const calculateTextHeight = (
             context: CanvasRenderingContext2D,
             text: string,
@@ -290,6 +295,7 @@ const PptxViewer: React.FC = () => {
               metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
             );
           };
+
           function drawTextWithWrapping(
             context: CanvasRenderingContext2D,
             text: string,
@@ -319,31 +325,14 @@ const PptxViewer: React.FC = () => {
             context.fillText(line, x, currentY);
           }
 
-          // 绘制带样式的文本元素
           elements.forEach(
             (
-              {
-                text,
-                fontSize,
-                color,
-                typeface,
-                fontWeight,
-                fontStyle,
-                pitchFamily,
-                charset,
-                x,
-                y,
-              },
+              { text, fontSize, color, typeface, fontWeight, fontStyle, x, y },
               index
             ) => {
               context.fillStyle = color;
-              context.font = `${fontStyle} ${fontWeight} ${fontSize} ${typeface} `;
+              context.font = `${fontStyle} ${fontWeight} ${fontSize} ${typeface}`;
 
-              // 记录字体信息以供调试
-              console.log(
-                `Text: ${text}, x: ${x}, y: ${y}, typeface: ${typeface}, fontWeight: ${fontWeight}, fontStyle: ${fontStyle}, color: ${color}, fontSize: ${fontSize}, PitchFamily: ${pitchFamily}, Charset: ${charset}`
-              );
-              // 检查是否与前一个元素的 y 相同
               if (
                 index > 0 &&
                 elements[index - 1].y === y &&
@@ -351,8 +340,8 @@ const PptxViewer: React.FC = () => {
               ) {
                 y = currentY;
               }
-              const maxWidth = canvas.width - x - 40; // 减去一些内边距
-              const lineHeight = parseInt(fontSize, 10) + 10; // 行高
+              const maxWidth = canvas.width - x - 40;
+              const lineHeight = parseInt(fontSize, 10) + 10;
               drawTextWithWrapping(
                 context,
                 text,
@@ -362,20 +351,17 @@ const PptxViewer: React.FC = () => {
                 lineHeight
               );
 
-              // 更新 currentY，避免重叠
               const textHeight = calculateTextHeight(context, text, fontSize);
-              currentY = y + textHeight + lineHeight + 10; // 10 是额外间距
+              currentY = y + textHeight + lineHeight + 10;
             }
           );
 
-          // 将画布转换为图片
           const slideImage = canvas.toDataURL("image/png");
 
           return {
             slideImage,
             elements: elements.map((e) => e.text).join("\n"),
             backgroundColor,
-            backgroundImage,
           };
         }
 
@@ -384,14 +370,10 @@ const PptxViewer: React.FC = () => {
 
       const contents = await Promise.all(slidePromises);
 
-      // 使用内容更新 UI
       setSlideImages(contents.map((content) => content?.slideImage || ""));
       setSlideContents(contents.map((content) => content?.elements || ""));
       setBackgroundStyles(
         contents.map((content) => content?.backgroundColor || "")
-      );
-      setBackgroundImages(
-        contents.map((content) => content?.backgroundImage || "")
       );
     } catch (error) {
       console.error("处理 PPTX 文件时出错:", error);
@@ -468,9 +450,6 @@ const PptxViewer: React.FC = () => {
             style={{
               flex: 0.5,
               backgroundColor: backgroundStyles[selectedSlideIndex],
-              backgroundImage: `url(${backgroundImages[selectedSlideIndex]})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
               width: "100%",
               padding: "10px",
               boxSizing: "border-box",
